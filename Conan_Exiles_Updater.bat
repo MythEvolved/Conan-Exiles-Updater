e8bb9195599694b64ea6c7fc23ee5c7834ecdb9c
KazuyaShuu <74711612+MythEvolved@users.noreply.github.com>
2021-05-28 15:32:41 +0300

    Update Conan_Exiles_Updater.bat

--- Conan_Exiles_Updater.bat
+++ Conan_Exiles_Updater.bat
@@ -3,4 +3,4 @@ call config.txt
 
 for /f "delims== tokens=1,2" %%G in (config.txt) do set %%G=%%H
 
-start steamcmd_path +login anonymous +force_install_dir server_path +app_update 443030 validate +quit
+start %steamcmd_path% +login anonymous +force_install_dir %server_path% +app_update 443030 validate +quit
